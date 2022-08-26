2c6b32d4284da2c550fd208d655f66c1dac7d865
Wisdom Ekpotu <whizfidel@gmail.com>
2022-08-26 15:55:16 +0100

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import LogoVPN from "../../public/assets/Logo.svg";
+import Logo from "../../public/assets/Logo.png";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="bg-white-300 pt-44 pb-24">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4 tour-footer">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <Image src={LogoVPN} className="h-8 w-auto mb-6" /> 
+          <Image src={Logo} className="h-8 w-auto mb-6" /> 
           <p className="mb-4">
             <strong className="font-medium">LaslesVPN</strong> is a private
             virtual network that has unique features and has high security.
