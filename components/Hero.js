6559a6aa55c711afd21c482f3b7d4ff82f29aa64
Wisdom Ekpotu <whizfidel@gmail.com>
2022-08-26 16:22:32 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -27,7 +27,7 @@ const Hero = ({
       <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
         <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
           <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-            Want anything to be easy with <strong>Logo Icon</strong>.
+            Want anything to be easy with <strong>LaslesVPN</strong>.
           </h1>
           <p className="text-black-500 mt-4 mb-6">
             Provide a network for all your needs with ease and fun using
